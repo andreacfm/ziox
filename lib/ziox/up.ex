3522f39e9a034f860f05ba76a9be63f5f2da916f
Andrea Campolonghi <acampolonghi@gmail.com>
2016-09-10 17:45:12 +0200

    Using Task async in up cmd

--- lib/ziox/up.ex
+++ lib/ziox/up.ex
@@ -2,6 +2,23 @@ defmodule Ziox.Up do
   require Logger
 
   def process(options) do
-    Logger.info("Running UP with options " <> inspect(options))
+    File.cd("/Users/andrea/dev/elixir")
+    {:ok, dirs} = File.ls
+
+    dirs
+    |> only_git
+    |> Enum.map(&Task.async(fn ->
+          {out, exit_code} = System.cmd("git", ["status"], cd: Path.expand(&1))
+          Logger.info "UP for #{Path.expand(&1)}\n" <> out
+        end))
+    |> Enum.map(fn (pid) -> Task.await(pid) end)
+
   end
+
+  def only_git(dirs) do
+    Enum.filter dirs, fn (dir) ->
+      File.dir?(dir) && File.exists?(dir <> "/.git")
+    end
+  end
+
 end
